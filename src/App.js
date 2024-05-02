03bca957f10b7b6352321d46e1dfa9798c4f71ac
Kokonod Ray <kokonodray2001@gmail.com>
2024-05-02 12:32:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./component/Header";
 import ListComponent from "./component/ListComponent";
 
+var count = 0;
 function App() {
   const [tasks, setTasks] = useState([
     "Read SpringBoot",
@@ -10,11 +11,41 @@ function App() {
     "Sleep for 2 hours",
     "Take a shower",
   ]);
+  const [strikes, setStrikes] = useState(Array(tasks.length).fill(false));
+  const [empty, setEmpty] = useState(true);
 
+  const taskButtonClickHndler = (index) => {
+    const newStriks = [...strikes];
+    newStriks[index] = !newStriks[index];
+    if (newStriks[index] === true) count++;
+    else count--;
+    if (count === 0) setEmpty(true);
+    else setEmpty(false);
+    setStrikes(newStriks);
+  };
+  const emptyButtonClickHandler = () => {
+    if (count === 0) {
+      setTasks([]);
+      return;
+    }
+
+    const newAllTask = tasks.filter((task, index) => strikes[index] === false);
+    setEmpty(true);
+    count = 0;
+    setTasks(newAllTask);
+    setStrikes(Array(tasks.length).fill(false));
+  };
   return (
     <div className='Application'>
       <Header />
-      <ListComponent allTasks={tasks} setAllTask={setTasks} />
+
+      <ListComponent
+        allTasks={tasks}
+        strikes={strikes}
+        empty={empty}
+        emptyButtonClickHandler={emptyButtonClickHandler}
+        taskButtonClickHndler={taskButtonClickHndler}
+      />
     </div>
   );
 }
--- src/component/ListComponent.jsx
+++ src/component/ListComponent.jsx
@@ -1,34 +1,10 @@
-import React, { useState } from "react";
-var count = 0;
-export default function ListComponent({ allTasks, setAllTask }) {
-  const [strikes, setStrikes] = useState(Array(allTasks.length).fill(false));
-  const [empty, setEmpty] = useState(true);
-
-  const taskButtonClickHndler = (index) => {
-    const newStriks = [...strikes];
-
-    newStriks[index] = !newStriks[index];
-    if (newStriks[index] === true) count++;
-    else count--;
-    if (count === 0) setEmpty(true);
-    else setEmpty(false);
-    setStrikes(newStriks);
-    console.log(count);
-  };
-  const emptyButtonClickHandler = () => {
-    if (count === 0) {
-      setAllTask([]);
-      return;
-    }
-
-    const newAllTask = allTasks.filter(
-      (task, index) => strikes[index] === false
-    );
-    setEmpty(true);
-    count = 0;
-    setAllTask(newAllTask);
-    setStrikes(Array(allTasks.length).fill(false));
-  };
+export default function ListComponent({
+  allTasks,
+  strikes,
+  empty,
+  emptyButtonClickHandler,
+  taskButtonClickHndler,
+}) {
   return (
     <div>
       <ul className='taskList'>
